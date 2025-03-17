020c494468a7eb3e0f9cc613d967204bd72c8f38
Marcelo Fernandes <132860265+Marcelo-122@users.noreply.github.com>
2025-03-16 22:54:33 -0300

    Bug fix and new functions
    -Fixed out of range error
    -added serializar_estado and calcular_utilidade functions

--- main.py
+++ main.py
@@ -30,4 +30,9 @@ while not game_over:
     if jogo.verificar_vitoria():
         game_over = True
 
+
+    print(jogo.serializar_estado())
+    print(jogo.calcular_utilidade(jogo.serializar_estado(),"J1"))
+    print(jogo.calcular_utilidade(jogo.serializar_estado(),"J2"))
+
     turn = (turn + 1) % 2
--- quoridor/game.py
+++ quoridor/game.py
@@ -116,18 +116,22 @@ class JogoQuoridor:
             print("Movimento fora dos limites!")
             return False
 
-        if direcao == "w" and linha > 0 and self.paredes_horizontais[linha - 1][coluna]:
+        if direcao == "w" and linha > 0 and linha - 1 < len(self.paredes_horizontais) and self.paredes_horizontais[linha - 1][coluna]:
             print("Há uma parede bloqueando o caminho!")
             return False
-        if direcao == "s" and self.paredes_horizontais[linha][coluna]:
+
+        if direcao == "s" and linha < len(self.paredes_horizontais) and self.paredes_horizontais[linha][coluna]:
             print("Há uma parede bloqueando o caminho!")
             return False
-        if direcao == "a" and coluna > 0 and self.paredes_verticais[linha][coluna - 1]:
+
+        if direcao == "a" and coluna > 0 and coluna - 1 < len(self.paredes_verticais[0]) and linha < len(self.paredes_verticais) and self.paredes_verticais[linha][coluna - 1]:
             print("Há uma parede bloqueando o caminho!")
             return False
-        if direcao == "d" and self.paredes_verticais[linha][coluna]:
+
+        if direcao == "d" and coluna < len(self.paredes_verticais[0]) and linha < len(self.paredes_verticais) and self.paredes_verticais[linha][coluna]:
             print("Há uma parede bloqueando o caminho!")
             return False
+
 
         self.jogadores[jogador] = (nova_linha, nova_coluna)
         return True
@@ -140,3 +144,46 @@ class JogoQuoridor:
             print("Player 2 venceu!")
             return True
         return False 
+
+    def serializar_estado(self):
+        estado = (
+            tuple(self.jogadores["J1"]),  # Posição do J1
+            tuple(self.jogadores["J2"]),  # Posição do J2
+            tuple(tuple(linha) for linha in self.paredes_horizontais),  # Paredes horizontais
+            tuple(tuple(linha) for linha in self.paredes_verticais),    # Paredes verticais
+            self.paredes_restantes["J1"],  # Paredes restantes para J1
+            self.paredes_restantes["J2"]   # Paredes restantes para J2
+        )
+        return estado
+    
+    def calcular_utilidade(self, estado, jogador):
+        # Extrai as informações do estado
+        posicao_j1, posicao_j2, paredes_horizontais, paredes_verticais, paredes_j1, paredes_j2 = estado
+        
+        # Define o objetivo de cada jogador
+        objetivo_j1 = 8  # J1 precisa chegar à linha 8
+        objetivo_j2 = 0  # J2 precisa chegar à linha 0
+        
+        # Calcula a distância do jogador até o objetivo
+        if jogador == "J1":
+            distancia_jogador = abs(posicao_j1[0] - objetivo_j1)  # Distância vertical de J1 até o objetivo
+            distancia_oponente = abs(posicao_j2[0] - objetivo_j2)  # Distância vertical de J2 até o objetivo
+            paredes_jogador = paredes_j1  # Paredes restantes de J1
+        else:
+            distancia_jogador = abs(posicao_j2[0] - objetivo_j2)  # Distância vertical de J2 até o objetivo
+            distancia_oponente = abs(posicao_j1[0] - objetivo_j1)  # Distância vertical de J1 até o objetivo
+            paredes_jogador = paredes_j2  # Paredes restantes de J2
+        
+        # Fatores de ponderação (ajuste conforme necessário)
+        peso_distancia_jogador = -1  # Quanto menor a distância, melhor
+        peso_distancia_oponente = 1  # Quanto maior a distância do oponente, melhor
+        peso_paredes = 10  # Ter mais paredes é vantajoso
+        
+        # Calcula a utilidade
+        utilidade = (
+            (peso_distancia_jogador * distancia_jogador) +  # Distância do jogador
+            (peso_distancia_oponente * distancia_oponente) +  # Distância do oponente
+            (peso_paredes * paredes_jogador)  # Paredes restantes
+        )
+        
+        return utilidade
